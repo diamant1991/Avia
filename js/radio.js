2030d93fa33c8dce50fbe8c4cde30f3bc5cce5d4
diamant1991 <diamant1988@mail.ru>
2015-07-14 16:54:11 +0300

    add other files

---
+++ js/radio.js
@@ -0,0 +1,20 @@
+// RadioButton
+$('.radioblock').find('.radio').each(function(){
+	$(this).click(function(){
+		var valueRadio = $(this).html();
+		$(this).parent().find('.radio').removeClass('active');
+		$(this).addClass('active');
+		$(this).parent().find('input').val(valueRadio);
+	});
+});
+
+$('.tableradio').find('.radio').each(function(){
+	$(this).click(function(){
+		var valueRadio = $(this).html();
+		$(this).parent().parent().parent().parent().find('.radio').removeClass('active');
+		$(this).addClass('active');
+		$(this).parent().parent().parent().parent().find('input').val(valueRadio);
+	});
+});
+
+
